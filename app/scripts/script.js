c7fcf9cdf622bda01371c230f169306dd4871d2a
Fernandoper92 <fernandoper92@gmail.com>
2021-04-02 04:51:28 -0300

    repositorio

---
+++ app/scripts/script.js
@@ -0,0 +1,12 @@
+
+let list = document.querySelectorAll('.nav-iten-box');
+
+for (let i = 0; i < list.length; i++) {
+    list[i].onclick = function() {
+        let j = 0;
+        while (j < list.length) {
+            list[j++].className = 'nav-iten-box';
+        }
+        list[i].className = 'nav-iten-box active'
+    }
+}
