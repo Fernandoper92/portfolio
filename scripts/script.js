4e921c1eafc393459d86ea13a8eb1c188ac0d242
Fernando Pereira <fernandoper92@gmail.com>
2021-05-20 02:28:18 -0300

    card dos projetos para mobile, tamanho das fontes.

--- scripts/script.js
+++ scripts/script.js
@@ -23,12 +23,11 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
-  }
+}
